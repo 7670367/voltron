da2c41f6146e25feef2c446bbef5da17d62e8230
7670367 <49253139+7670367@users.noreply.github.com>
2019-10-17 19:00:53 -0500

    hic copy script

--- centrifuge_slurm_scripts/copy_HIC_folders.sh
+++ centrifuge_slurm_scripts/copy_HIC_folders.sh
@@ -1,31 +1,61 @@
-#! /bin/base
+#! /bin/bash
 
-cp /mnt/HIC4000/MiSeq/HIC11383/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11383/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11489/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11489/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11490/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11490/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11491/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11491/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11492/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11492/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11493/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11493/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11494/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11494/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11495/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11495/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11496/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11496/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11497/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11497/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11498/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11498/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11499/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11499/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11500/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11500/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11501/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11501/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11502/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11502/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11503/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11503/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11504/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11504/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11505/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11505/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11506/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11506/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11507/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11507/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11508/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11508/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11509/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11509/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11510/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11510/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11511/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11511/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11512/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11512/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11513/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11513/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11514/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11514/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11515/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11515/fastq/
-cp /mnt/HIC4000/MiSeq/HIC11516/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11516/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11383/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11489/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11490/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11491/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11492/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11493/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11494/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11495/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11496/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11497/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11498/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11499/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11500/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11501/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11502/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11503/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11504/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11505/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11506/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11507/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11508/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11509/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11510/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11511/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11512/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11513/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11514/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11515/fastq/
+mkdir -p /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11516/fastq/
+
+# cp /mnt/HIC4000/MiSeq/HIC11383/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11383/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11489/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11489/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11490/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11490/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11491/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11491/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11492/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11492/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11493/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11493/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11494/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11494/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11495/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11495/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11496/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11496/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11497/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11497/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11498/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11498/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11499/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11499/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11500/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11500/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11501/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11501/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11502/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11502/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11503/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11503/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11504/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11504/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11505/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11505/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11506/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11506/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11507/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11507/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11508/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11508/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11509/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11509/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11510/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11510/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11511/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11511/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11512/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11512/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11513/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11513/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11514/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11514/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11515/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11515/fastq/
+# cp /mnt/HIC4000/MiSeq/HIC11516/fastq/* /gpfs0/work/joshua.theisen/git/voltron/hics/HIC11516/fastq/
