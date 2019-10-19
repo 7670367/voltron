eca097c1f2b2fb6eb31b5372f01ae5b7d3858a99
7670367 <49253139+7670367@users.noreply.github.com>
2019-10-19 10:21:09 -0500

    fixed centrifuge

---
+++ centrifuge_slurm_scripts/cat_top.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+touch all_top.txt
+echo HIC11490.centrep >> all_top.txt && head -1 HIC11490.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11490.centrep | head -10 >> all_top.txt
+echo HIC11490.centrep >> all_top.txt && head -1 HIC11490.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11490.centrep | head -10 >> all_top.txt
+echo HIC11491.centrep >> all_top.txt && head -1 HIC11491.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11491.centrep | head -10 >> all_top.txt
+echo HIC11492.centrep >> all_top.txt && head -1 HIC11492.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11492.centrep | head -10 >> all_top.txt
+echo HIC11493.centrep >> all_top.txt && head -1 HIC11493.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11493.centrep | head -10 >> all_top.txt
+echo HIC11494.centrep >> all_top.txt && head -1 HIC11494.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11494.centrep | head -10 >> all_top.txt
+echo HIC11495.centrep >> all_top.txt && head -1 HIC11495.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11495.centrep | head -10 >> all_top.txt
+echo HIC11496.centrep >> all_top.txt && head -1 HIC11496.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11496.centrep | head -10 >> all_top.txt
+echo HIC11497.centrep >> all_top.txt && head -1 HIC11497.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11497.centrep | head -10 >> all_top.txt
+echo HIC11498.centrep >> all_top.txt && head -1 HIC11498.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11498.centrep | head -10 >> all_top.txt
+echo HIC11499.centrep >> all_top.txt && head -1 HIC11499.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11499.centrep | head -10 >> all_top.txt
+echo HIC11500.centrep >> all_top.txt && head -1 HIC11500.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11500.centrep | head -10 >> all_top.txt
+echo HIC11501.centrep >> all_top.txt && head -1 HIC11501.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11501.centrep | head -10 >> all_top.txt
+echo HIC11502.centrep >> all_top.txt && head -1 HIC11502.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11502.centrep | head -10 >> all_top.txt
+echo HIC11503.centrep >> all_top.txt && head -1 HIC11503.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11503.centrep | head -10 >> all_top.txt
+echo HIC11504.centrep >> all_top.txt && head -1 HIC11504.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11504.centrep | head -10 >> all_top.txt
+echo HIC11505.centrep >> all_top.txt && head -1 HIC11505.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11505.centrep | head -10 >> all_top.txt
+echo HIC11506.centrep >> all_top.txt && head -1 HIC11506.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11506.centrep | head -10 >> all_top.txt
+echo HIC11507.centrep >> all_top.txt && head -1 HIC11507.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11507.centrep | head -10 >> all_top.txt
+echo HIC11508.centrep >> all_top.txt && head -1 HIC11508.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11508.centrep | head -10 >> all_top.txt
+echo HIC11509.centrep >> all_top.txt && head -1 HIC11509.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11509.centrep | head -10 >> all_top.txt
+echo HIC11510.centrep >> all_top.txt && head -1 HIC11510.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11510.centrep | head -10 >> all_top.txt
+echo HIC11511.centrep >> all_top.txt && head -1 HIC11511.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11511.centrep | head -10 >> all_top.txt
+echo HIC11512.centrep >> all_top.txt && head -1 HIC11512.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11512.centrep | head -10 >> all_top.txt
+echo HIC11513.centrep >> all_top.txt && head -1 HIC11513.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11513.centrep | head -10 >> all_top.txt
+echo HIC11514.centrep >> all_top.txt && head -1 HIC11514.centrep >> all_top.txt && sort -rt $'\t' -nk5 HIC11514.centrep | head -10 >> all_top.txt
