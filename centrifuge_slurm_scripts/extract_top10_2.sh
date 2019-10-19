ea626476700fdb793901fdee42cbeace4cd89241
7670367 <49253139+7670367@users.noreply.github.com>
2019-10-19 09:16:26 -0500

    extract

---
+++ centrifuge_slurm_scripts/extract_top10_2.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
+column -ts $'\t' HIC11489.centrep | head -1 > HIC11489_top.txt && sort -rt $'\t' -nk5 HIC11489.centrep | column -ts $'\t' | head -10 | cat >> HIC11489_top.txt
